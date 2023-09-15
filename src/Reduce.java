77aa3792e9e883b83077e0c9285b72a4eb59bdb2
Razan Ahsan Rifandi <razan.rifandi@mail.utoronto.ca>
2023-09-16 02:52:53 +0700

    added Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100, ans = 0;
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i /= 2;
+            } else {
+                i--;
+            }
+            ans++;
+        }
+        System.out.println(ans);;
+    }
 }
